84a4fb3cf78876fd2359dcd9732f11ae9ef00721
Patrick Mason <patrickmason73@gmail.com>
2022-06-27 19:45:09 -0600

    commit tree cc1183691e9bc6d478dde21fc0eef0c76d80c3ba

---
+++ index.js
@@ -0,0 +1,28 @@
+function shout(string) {
+    return string.toUpperCase();
+}
+function whisper(string) {
+    return string.toLowerCase();
+}
+function logShout(string) {
+    console.log(string.toUpperCase());
+}
+logWhisper(string.toUpperCase);
+function logWhisper(string) {
+    console.log(string.toLowerCase());
+}
+logWhisper(string.toLowerCase);
+function sayHiToHeadphonedRoommate(string) {
+    if (string.toLowerCase(string) === string) {
+    return "I can't hear you!";
+    }
+    else if (string.toUpperCase(string) === string) {
+        return "YES INDEED!";
+    }
+    else if ("Let's have dinner together!" === string){
+        return "I would love to!";
+    }
+}
+sayHiToHeadphonedRoommate(string.toLowerCase);
+sayHiToHeadphonedRoommate(string.toUpperCase);
+sayHiToHeadphonedRoommate(string === "Let's have dinner together!");
